36c579a5d12ded2723a666b4960ee667e841a7de
chen luo <rackingroll@163.com>
2015-07-20 08:47:43 +0800

    Fix Bugs!

--- HetePathMine.m
+++ HetePathMine.m
@@ -9,6 +9,7 @@
 [P1,P2,P3,P4,P5,P6,P7,P8,P9,P10] = GetStructureMatrix(AP,CP,PP,TP) ;
 
 %Initialization
+[userGuide,groundtruth] = GetSeedMatrix(0.1) ;
 %RandWalk,SymRandWalk,Normalization,PathSim
 PS1 = PathSim(P1) ; 
 PS2 = PathSim(P2) ;
@@ -20,7 +21,7 @@ PS7 = PathSim(P7) ;
 PS8 = PathSim(P8) ;
 PS9 = PathSim(P9) ;
 PS10 = PathSim(P10) ; 
-[PT1,PT2,PT3,PT4,PT5,PT6,PT7,PT8,PT9,PT10,guide] = TrainingMetaPath(PS1,PS2,PS3,PS4,PS5,PS6,PS7,PS8,PS9,PS10,uGuide) ;
+[PT1,PT2,PT3,PT4,PT5,PT6,PT7,PT8,PT9,PT10,guide] = TrainingMetaPath(PS1,PS2,PS3,PS4,PS5,PS6,PS7,PS8,PS9,PS10,userGuide) ;
 [PT1,PT2,PT3,PT4,PT5,PT6,PT7,PT8,PT9,PT10,guide] = RemoveNoise(PT1,PT2,PT3,PT4,PT5,PT6,PT7,PT8,PT9,PT10,guide) ;
 
 % Path Selection Model, Using Linear result as the MLE Input
@@ -31,7 +32,6 @@ A = Mle(PT1,PT2,PT3,PT4,PT5,PT6,PT7,PT8,PT9,PT10,guide,lanmda0) ;
 resultMatrix = GetResultMatrixClass (A,PS1,PS2,PS3,PS4,PS5,PS6,PS7,PS8,PS9,PS10);
 size_of_author = size(resultMatrix) ;
 resultMatrix = resultMatrix + ones(size_of_author);
-[userGuide,groundtruth] = GetSeedMatrix(0.1) ;
 author_label = zeros(size(userGuide,1),1) ;
 for j=1:size(userGuide,1)
 	if find(userGuide(j,:)~=0) 
@@ -52,4 +52,4 @@ lamda = [2];
 [result score] = GNetMine(W, WTypes, label, alpha, lamda);
 clusterResult = result{1} ;
 
-ac = Accuracy(clusterResult,groundtruth) ;
+ac = Accuracy(clusterResult,groundtruth) 
